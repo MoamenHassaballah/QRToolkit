40d4d4da6a54a6ef16c2401f7a9bbc5ddb99431e
Moamen Hassaballah <hassaballahmoamen84@gmail.com>
2021-01-19 16:41:25 +0200

    Main Screen Almost Done

--- app/src/main/java/com/moaapps/qrtoolkit/MainActivity.kt
+++ app/src/main/java/com/moaapps/qrtoolkit/MainActivity.kt
@@ -2,10 +2,18 @@ package com.moaapps.qrtoolkit
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import com.moaapps.qrtoolkit.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     companion object{
         @JvmStatic
         fun start(context: Context) {
@@ -13,8 +21,30 @@ class MainActivity : AppCompatActivity() {
             context.startActivity(starter)
         }
     }
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawer, binding.toolbar, R.string.drawer_open, R.string.drawer_close)
+        binding.drawer.addDrawerListener(toggle)
+//        toggle.setHomeAsUpIndicator(R.drawable.ic_menu)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navView.setCheckedItem(R.id.home)
+        binding.navView.setNavigationItemSelectedListener(this)
     }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        binding.navView.setCheckedItem(item)
+        binding.drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
 }
